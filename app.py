75d90e8d49a4c8a4682bce2a72d5bf6e768450d9
Simon Jäger <simon.jager@live.com>
2019-05-28 19:17:29 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import uuid
+import subprocess
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/api/stl/thumb', methods=['GET', 'POST'])
+def get_stl_thumbnail():
+    # Create SCAD file.
+    session_id = uuid.uuid1()
+    stl_file = 'test.stl'
+    scad_file = '{}.scad'.format(session_id)
+    png_file = '{}.png'.format(session_id)
+
+    with open(scad_file) as file:
+        file.write('import({});'.format(stl_file))
+
+    # Render image.
+    res = subprocess.check_output(['openscad', '-o', png_file, scad_file])
+    print(res)
+
+    return jsonify({'task': res}), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
